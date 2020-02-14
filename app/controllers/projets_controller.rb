2dcc37a10e74a757734c35e6b5ce7b4fc62f9885
BeAnjara <beanjarahoussen@gmail.com>
2020-02-14 10:02:09 +0300

    filtre

--- app/controllers/projets_controller.rb
+++ app/controllers/projets_controller.rb
@@ -6,8 +6,8 @@ class ProjetsController < ApplicationController
 
        #recherche controller globale(titre du projet)
                       if params[:search]
-                        if params[:search].blank?  
-                          redirect_to(root_path, alert: "Empty field!") 
+                        if params[:search] == 1
+                            redirect_to(root_path)
                         else  
                           @parameter = params[:search].downcase  
                           @results = Projet.all.where("lower(nom_du_projet) LIKE :search", search: "%#{@parameter}%")  
@@ -16,8 +16,8 @@ class ProjetsController < ApplicationController
               #recherhe controller ptf
                      if params[:ptf_id]
                           Ptf.find(params[:ptf_id]).projets
-                        if params[:ptf_id].blank?  
-                          redirect_to(root_path, alert: "Empty field!") 
+                        if params[:ptf_id]== 1
+                               redirect_to(root_path)
                         else  
                           @parameter = params[:ptf_id].downcase  
                           @ptf = Projet.all.where(ptf_id: params[:ptf_id])  
@@ -30,8 +30,9 @@ class ProjetsController < ApplicationController
                           puts "*"* 100
                           Montant.find(params[:montant_id]).projets
                            puts "*"* 100
-                        if params[:montant_id].blank?  
-                          redirect_to(root_path, alert: "Empty field!") 
+                        if params[:montant_id] == 1 
+                           redirect_to(root_path)
+                         
                         else  
                           @parameter = params[:montant_id].downcase  
                           @montant = Projet.all.where(montant_id: params[:montant_id])  
@@ -42,8 +43,9 @@ class ProjetsController < ApplicationController
                           puts "*"* 100
                           Appui.find(params[:appui_id]).projets
                            puts "*"* 100
-                        if params[:appui_id].blank?  
-                          redirect_to(root_path, alert: "Empty field!") 
+                        if params[:appui_id]== 1 
+                           redirect_to(root_path) 
+                         
                         else  
                           @parameter = params[:appui_id].downcase  
                           @appui = Projet.all.where(appui_id: params[:appui_id])  
@@ -51,8 +53,9 @@ class ProjetsController < ApplicationController
                     end 
                #recherche controller  appui
                         if params[:fin]
-                        if params[:fin].blank?  
-                          redirect_to(root_path, alert: "Empty field!") 
+                        if params[:fin] == 1
+                           redirect_to(root_path)
+                         
                         else  
                           @parameter = params[:fin].downcase  
                           @fin = Projet.all.where(fin: params[:fin])  
@@ -63,8 +66,9 @@ class ProjetsController < ApplicationController
               #recherche controller filiere
 
                     if params[:search_filiere]
-                      if params[:search_filiere].blank?
-                        redirect_to(root_path, alert: "select")
+                      if params[:search_filiere]== 1
+                         redirect_to(root_path)
+                       
                       else
                         @parameter = params[:search_filiere].downcase
                         @filiere = Filiereprojet.all.where(filiere_id: params[:search_filiere])
@@ -74,8 +78,9 @@ class ProjetsController < ApplicationController
               #recherche controller zone
 
                        if params[:search_zone]
-                          if params[:search_zone].blank?
-                              redirect_to(root_path, alert: "select")
+                          if params[:search_zone]== 1
+                             redirect_to(root_path)
+                             
                            else
                               @parameter = params[:search_zone].downcase
                               @res = Zoneprojet.all.where(zone_id: params[:search_zone])               
@@ -84,8 +89,9 @@ class ProjetsController < ApplicationController
                #recherche controller beneficiaire
 
                        if params[:search_ben]
-                          if params[:search_ben].blank?
-                              redirect_to(root_path, alert: "select")
+                          if params[:search_ben]== 1
+                             redirect_to(root_path)
+                              
                            else
                               @parameter = params[:search_ben].downcase
                               @ben = Benefprojet.all.where(beneficiaire_id: params[:search_ben])               
@@ -95,8 +101,9 @@ class ProjetsController < ApplicationController
               #recherche controller IGA
 
                       if params[:search_iga]
-                          if params[:search_iga].blank?
+                          if params[:search_iga]== 1
                              redirect_to(root_path)
+                             
                          else
                            @parameter = params[:search_iga]
                            @search = Igaprojet.all.where(iga_id: params[:search_iga])
@@ -105,8 +112,9 @@ class ProjetsController < ApplicationController
               #recherche controller secteur
 
                       if params[:search_secteur]
-                          if params[:search_secteur].blank?
+                          if params[:search_secteur]== 1
                              redirect_to(root_path)
+                             
                             
                           else
                              @parameter = params[:search_secteur]
