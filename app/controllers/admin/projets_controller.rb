0dc4b4aa72b8c6a5d6181f1c4ad3c9fcf2a8152b
Rianasoa <rrianasoaonimalala@gmail.com>
2020-02-27 17:22:36 +0300

    modifier projet

--- app/controllers/admin/projets_controller.rb
+++ app/controllers/admin/projets_controller.rb
@@ -20,7 +20,7 @@ class Admin::ProjetsController < ApplicationController
                 forme_d_appui: params[:forme_d_appui],
                 debut_du_projet: params[:debut_du_projet],
                 fin: params[:fin],
-                apdem: params[:apdem], bailleur_id: current_user.id)
+                apdem: params[:apdem], lien: params[:lien], bailleur_id: current_user.id)
                      
                   @secteur_ids = params[:secteur_ids]
                 @secteur_ids.each do |x|
@@ -66,24 +66,47 @@ class Admin::ProjetsController < ApplicationController
     @secteurprojets.each do |sp|
       @sects << sp.secteur
     end
+
+    @filiereprojets = Filiereprojet.where(projet_id: @projet.id)
+    @fils = []
+    @filiereprojets.each do |fp|
+      @fils << fp.filiere
+    end
+
+    @beneficiaireprojets = Benefprojet.where(projet_id: @projet.id)
+    @bens = []
+    @beneficiaireprojets.each do |bp|
+      @bens << bp.beneficiaire
+    end
+
+    @igaprojets = Igaprojet.where(projet_id: @projet.id)
+    @igas = []
+    @igaprojets.each do |ip|
+      @igas << ip.iga
+    end
+
+    @zoneprojets = Zoneprojet.where(projet_id: @projet.id)
+    @zones = []
+    @zoneprojets.each do |zp|
+      @zones << zp.zone
+    end
+
   end
 
   def update
-  	@project = Projet.find(params[:id])
-         
-         if @project.update(nom_du_projet: params[:nom_du_projet],
+         puts "*"*100
+  	@projet = Projet.find(params[:id],)
+         if @projet.update(nom_du_projet: params[:nom_du_projet],
               ptf_id: params[:ptf_id],
               forme_d_appui: params[:forme_d_appui],
               objectif_generale_du_projet: params[:objectif_generale_du_projet],
               aspsp: params[:aspsp],
-              partenaire_d_implementaton: params[:partenaire_d_implementaton],
-              beneficiaires: params[:Beneficiaires],montant: params[:montant],
-              nature_de_l_appui: params[:nature_de_l_appui],
+              partenaire_d_implementaton: params[:partenaire_d_implementaton],montant: params[:montant],
               debut_du_projet: params[:debut_du_projet],
               fin: params[:fin],
               apdem: params[:apdem], bailleur_id: current_user.id)
               
-          puts "*"*100
+          
            puts params[:sects]
             puts "*"*100
               params[:sects].each do |s|
@@ -95,24 +118,47 @@ class Admin::ProjetsController < ApplicationController
                     Secteurprojet.create(projet_id: @projet.id, secteur_id: params[:"secteur_#{x}"])
                 end
               end
-              
-              # @filiere_ids = params[:filiere_ids]
-              #   @filiere_ids.each do |x|
-              #     Filiereprojet.update(projet_id: @project.id , filiere_id: x.to_i)
-              #   end
-              # @iga_ids = params[:iga_ids]
-              #   @iga_ids.each do |i|
-              #     Igaprojet.update(projet_id: @project.id , iga_id: i.to_i) 
-              #   end
-              # @zone_ids = params[:zone_ids]
-              #   @zone_ids.each do |z|
-              #     Zoneprojet.update(projet_id: @project.id , zone_id: z.to_i)
-              #   end
-              # @beneficiaire_ids = params[:beneficiaire_ids]
-              #   @beneficiaire_ids.each do |b|
-              #     Benefprojet.update(projet_id: @project.id , beneficiaire_id: b.to_i)
-              #   end
 
+              params[:fils].each do |f|
+
+                Filiereprojet.where(filiere_id: f.id, projet_id: @projet.id).destroy_all
+              end
+              for x in 0..Filiere.all.count-1
+                if params[:"filiere_#{x}"]
+                    Filiereprojet.create(projet_id: @projet.id, filiere_id: params[:"filiere_#{x}"])
+                end
+              end
+
+              params[:bens].each do |b|
+
+                Benefprojet.where(beneficiaire_id: b.id, projet_id: @projet.id).destroy_all
+              end
+              for x in 0..Beneficiaire.all.count-1
+                if params[:"beneficiaire_#{x}"]
+                    Benefprojet.create(projet_id: @projet.id, beneficiaire_id: params[:"beneficiaire_#{x}"])
+                end
+              end
+
+              params[:igas].each do |i|
+
+                Igaprojet.where(iga_id: i.id, projet_id: @projet.id).destroy_all
+              end
+              for x in 0..Iga.all.count-1
+                if params[:"iga_#{x}"]
+                    Igaprojet.create(projet_id: @projet.id, iga_id: params[:"iga_#{x}"])
+                end
+              end
+
+
+              params[:zones].each do |z|
+
+                Zoneprojet.where(zone_id: z.id, projet_id: @projet.id).destroy_all
+              end
+              for x in 0..Zone.all.count-1
+                if params[:"zone_#{x}"]
+                    Zoneprojet.create(projet_id: @projet.id, zone_id: params[:"zone_#{x}"])
+                end
+              end
                 redirect_to  admin_projet_path
              else
                 render :edit     
