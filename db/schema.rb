9f604f179a0ea55777097335b3ba2f4c5e0317dc
Rianasoa <rrianasoaonimalala@gmail.com>
2020-03-09 14:33:41 +0300

    end

--- db/schema.rb
+++ db/schema.rb
@@ -150,12 +150,10 @@ ActiveRecord::Schema.define(version: 2020_03_09_063703) do
     t.bigint "appui_id"
     t.bigint "ptf_id"
     t.bigint "bailleur_id"
-    t.bigint "montant_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["appui_id"], name: "index_projets_on_appui_id"
     t.index ["bailleur_id"], name: "index_projets_on_bailleur_id"
-    t.index ["montant_id"], name: "index_projets_on_montant_id"
     t.index ["ptf_id"], name: "index_projets_on_ptf_id"
   end
 
