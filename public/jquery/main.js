60cea5e45a24a029621d15792305eb5b4cd6b8a3
Rianasoa <rrianasoaonimalala@gmail.com>
2020-03-16 16:59:10 +0300

    montant

--- public/jquery/main.js
+++ public/jquery/main.js
@@ -5,10 +5,11 @@ var thumbLeft = document.querySelector(".slider > .thumb.left");
 var thumbRight = document.querySelector(".slider > .thumb.right");
 var range = document.querySelector(".slider > .range");
 var values = [0,5,10,40,100,"+"];
-var outputLeft = document.getElementById('output-left');
-var outputRight = document.getElementById('output-right');
+var outputLeft = document.getElementById('outputleft');
+var outputRight = document.getElementById('outputright');
 var ir = document.getElementById('ir');
 var il = document.getElementById('il');
+var showMontant = document.getElementById('showMontant');
 
 function setLeftValue(){
     var _this = inputLeft,
@@ -35,15 +36,75 @@ setRightValue();
 inputLeft.addEventListener("input", setLeftValue);
 inputRight.addEventListener("input", setRightValue);
 
-// VALEUR DE LA RANGE
+//VALEUR DE LA RANGE
 function valueLeft(){
-    outputLeft.innerHTML = values[this.value];
-    il.value = values[this.value];
+    if (outputRight.value == "" || outputRight.value == "+") {
+        outputLeft.innerHTML = values[this.value];
+        if (outputLeft.value == "0") {
+        outputLeft.innerHTML = values[this.value];
+
+        showMontant.innerHTML = "montant en millions de USD";
+        }
+        else {
+        outputLeft.innerHTML = values[this.value];
+        showMontant.innerHTML = "plus de " + outputLeft.value + " millions d'usd";
+        ir.value = "max";
+        il.value = outputleft.value;
+        }
+    }
+    else{
+        outputLeft.innerHTML = values[this.value];
+        if (outputLeft.value == "0") {
+        outputLeft.innerHTML = values[this.value];
+
+            showMontant.innerHTML = "moins de " + outputRight.value + " millions d'usd";
+            il.value = 0;
+            ir.value = outputRight.value;
+        }
+        else{
+            outputLeft.innerHTML = values[this.value];
+            otl.innerHTML = " et "
+            il.value = values[this.value];
+            showMontant.innerHTML = "entre" + " " + outputLeft.value + " " + "millions et " + " " + outputRight.value + " " + "millions d'USD";
+            ir.value = outputRight.value;
+        }
+    }
 };
 function valueRight(){
-    outputRight.innerHTML = values[this.value];
-    ir.value = values[this.value];
+    if (outputLeft.value == "" || outputLeft.value == "0") {
+        outputRight.innerHTML = values[this.value];
+        if (outputRight.value == "+") {
+            outputRight.innerHTML = values[this.value];
+        showMontant.innerHTML = "montant en millions de USD";
+        }
+        else {
+            outputRight.innerHTML = values[this.value];
+        showMontant.innerHTML = "moins de " + outputRight.value + " millions d'usd";
+        il.value = 0;
+        ir.value = outputRight.value;
+        }
+    }
+    else{
+        outputRight.innerHTML = values[this.value];
+        if (outputRight.value == "+") {
+            outputRight.innerHTML = values[this.value];
+        showMontant.innerHTML = "plus de " + outputLeft.value+ " millions d'usd";
+        il.value = outputLeft.value;
+        ir.value = "max";
+        }
+        else {
+            outputRight.innerHTML = values[this.value];
+            otl.innerHTML = " et "
+            ir.value = values[this.value];
+            showMontant.innerHTML = "entre" + " " + outputLeft.value + " " + "millions et " + " " + outputRight.value + " " + "millions d'USD";
+            il.value = outputLeft.value;
+        }
+    }
 };
+
+
+
+
 inputRight.addEventListener('input', valueRight)
 inputLeft.addEventListener('input', valueLeft)
 
