0561baa86b6ff915f8e7e1b0b0532729718599dc
itokiana <itokianarajoelison@gmail.com>
2020-04-07 01:13:04 +0300

    DEPLOY by Itokiana

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,9 +48,8 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 # Only keep the last 5 releases to save disk space
 # append :linked_files, 'config/database.yml', 'config/master.key'
+append :linked_files, 'config/secrets.yml'
 set :keep_releases, 5
-
-set :secret_key_base, "#{ENV['SECRET_KEY_BASE']}"
 
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
 # This is useful if you don't want to use ENV variables
