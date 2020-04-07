b69a7517c2380ba2caa20a617ad73db557c2cfed
safidypatrick <safidypatrick8gmail.com>
2020-04-07 15:04:30 +0300

    pa2

--- app/controllers/super_admin/users_controller.rb
+++ app/controllers/super_admin/users_controller.rb
@@ -5,7 +5,8 @@ class SuperAdmin::UsersController < ApplicationController
 
 	def index
 		@users = User.where(is_admin: true).or(User.where(is_consultant: true))
-		@users_en_attente = User.where(is_consultant: false,is_super_admin: false,is_admin: nil)	
+		@users_en_attente = User.where(is_consultant: false,is_super_admin: false,is_admin: nil)
+	
 	end
 
 	def show
