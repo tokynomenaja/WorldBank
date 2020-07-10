2aeea2111e4b25f31483e9556f8f1bec28934d6e
tokynomenaja <nomenjatoky21@gmail.com>
2020-07-10 11:08:30 +0300

    test heroku 3

--- db/schema.rb
+++ db/schema.rb
@@ -372,7 +372,7 @@ ActiveRecord::Schema.define(version: 2020_07_10_072716) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.boolean "is_signed_in"
+    t.boolean "is_signed_in", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
