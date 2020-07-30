9eaa7cd08f5c97f2f53e51d7df2a55a35fe4791d
itokiana <itokianarajoelison@gmail.com>
2020-07-30 13:56:25 +0300

    Add sendmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ config.action_mailer.delivery_method = :sendmail
 # Defaults to:
 config.action_mailer.sendmail_settings = {
   location: '/usr/sbin/sendmail'
-#   arguments: '-i'
+  arguments: '-i'
 }
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
