f9a5cf61f1ae64ca3a60d76d5dbbc25220964b76
Rianasoa <rrianasoaonimalala@gmail.com>
2020-08-07 20:11:55 +0300

    mail sender

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,23 +3,24 @@ class UserMailer < ApplicationMailer
 	def welcome_email(user)
 	    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
 	    @user = user 
-
+	    @admin = User.find(30)
 	    #on définit une variable @url qu'on utilisera dans la view d’e-mail
 	    @url  = 'http://plateforme-spi.mg/users/sign_in' 
 
 	    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-	    mail(to: @user.email, subject: 'Email de validation de compte bailleur') 
+	    mail(to: [@user.email,@admin.email], subject: 'Email de validation de compte bailleur') 
 	end
 
 	def welcome_visit(user)
 	    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
 	    @user = user 
+	    @admin = User.find(30)
 
 	    #on définit une variable @url qu'on utilisera dans la view d’e-mail
 	    @url  = 'http://plateforme-spi.mg/users/sign_in' 
 
 	    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-	    mail(to: @user.email, subject: 'Email de validation de compte visiteur') 
+	    mail(to: [@user.email,@admin.email], subject: 'Email de validation de compte visiteur') 
 	end
 
 	def create_project_email(project)
@@ -29,12 +30,13 @@ class UserMailer < ApplicationMailer
 	    @id = @project.id
 
 	    @user = @project.bailleur
+	    @admin = User.find(30)
 
 	    #on définit une variable @url qu'on utilisera dans la view d’e-mail
 	    @url  = "http://plateforme-spi.mg/admin/projets/#{@id}"
 
 	    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-	    mail(to: @user.email, subject: 'Email de validation de projet ') 
+	    mail(to: [@user.email,@admin.email], subject: 'Email de validation de projet ') 
 	end
 
 	def email_validation(project)
@@ -43,12 +45,13 @@ class UserMailer < ApplicationMailer
 	    @id = @project.id
 
 	    @user = @project.bailleur
+	    @admin = User.find(30)
 
 	    #on définit une variable @url qu'on utilisera dans la view d’e-mail
 	    @url  = "http://plateforme-spi.mg/admin/projets/#{@id}"
 
 	    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-	    mail(to: @user.email, subject: 'SECTEUR PRIVE-FINANCE : Fiche Projet Validée') 
+	    mail(to: [@user.email,@admin.email], subject: 'SECTEUR PRIVE-FINANCE : Fiche Projet Validée') 
 		
 	end
 
@@ -59,12 +62,13 @@ class UserMailer < ApplicationMailer
 	    @id = @project.id
 
 	    @user = @project.bailleur
+	    @admin = User.find(30)
 
 	    #on définit une variable @url qu'on utilisera dans la view d’e-mail
 	    @url  = "http://plateforme-spi.mg/admin/projets/#{@id}"
 
 	    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-	    mail(to: @user.email, subject: 'SECTEUR PRIVE-FINANCE : Fiche Projet Rejetée') 
+	    mail(to: [@user.email,@admin.email], subject: 'SECTEUR PRIVE-FINANCE : Fiche Projet Rejetée') 
 		
 	end
 
