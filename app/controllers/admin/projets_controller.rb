538c59a8687d65268716afdcd9700855bde38cbc
itokiana <itokianarajoelison@gmail.com>
2020-10-13 15:56:29 +0300

    Modif projet par institution

--- app/controllers/admin/projets_controller.rb
+++ app/controllers/admin/projets_controller.rb
@@ -20,10 +20,10 @@ class Admin::ProjetsController < ApplicationController
       # end
       organisme = Organisme.where(user_id: current_user.id)[0]
       if organisme.ptf_id != nil
+        p "##################################"
+        p Projet.where(ptf_id: organisme.ptf_id).order(id: :desc)
+        p "##################################"
         @projets = Projet.where(ptf_id: organisme.ptf_id).order(id: :desc).page(params[:page]).per(9)
-        p "##################################"
-        p @projets
-        p "##################################"
       elsif organisme.iga_id != nil
         @projets = Projet.joins(:igaprojets).where(igaprojets: { iga_id: organisme.iga_id }).page(params[:page]).per(9)
       else
